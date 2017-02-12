bbbeee99c55984045f4a7d9b7e960425cb2808b2
Amena <ame1.khatun@gmail.com>
2017-02-12 15:47:56 -0500

    added in all the get actions

--- lib/guide.rb
+++ lib/guide.rb
@@ -1,6 +1,11 @@
 require 'restaurant'
 class Guide
 
+  class Config
+    @@actions = ['list', 'find', 'quit']
+    def self.actions; @@actions end
+  end
+
   def initialize(path=nil)
       #locate the restaurant text file at path
     Restaurant.filepath = path
@@ -14,18 +19,48 @@ class Guide
       puts "exiting"
       exit!
     end
-
-
   end
+
   def launch!
     introduction
     #action loop
-    #whatdo you want to do? (list, find, add, quit)
-    #do that action
-    #repeat until user quits
+    result = nil
+    until result == :quit
+      action = get_action
+      result = do_action(action)
+      #whatdo you want to do? (list, find, add, quit)
+      #do that action
+      #repeat until user quits
+     end
     conclusion
   end
 
+  def get_action
+    action = nil
+    #keep asking for user input until we get a valid action
+    until Guide::Config.actions.include?(action)
+    puts "Actions:" + Guide::Config.actions.join(",") if action
+    print ">"
+    user_response = gets.chomp
+    action = user_response.downcase.strip
+    end
+    return action
+  end
+
+  def do_action(action)
+    case action
+    when 'list'
+      puts "Listing...."
+    when 'find'
+      puts "Finding..."
+    when "add"
+      puts "Adding..."
+    when 'quit'
+      return :quit
+    else
+      puts "I don't understand that command."
+    end
+  end
   def introduction
     puts "Welcome to the Food Finder"
     puts "This is an interactive guide to help you find the food you crave"
--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -23,6 +23,8 @@ class Restaurant
 
   def self.create_file
     #create the restaurant file
+    File.open(@@filepath, 'w') unless file_exists?
+    return file_usable?
   end
 
   def self.saved_restaurants
